3dfb3dd03b77d7a21a111d4187c4472e3bb51a34
= <matthewcb11@gmail.com>
2020-06-12 16:54:15 -0600

    Progress bar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	// handleChange(event) {
+	// 	if (event.target.value === '') {
+	// 		this.setState({ songs: undefined });
+	// 		return;
+	// 	}
+	// 	this.setState({ searchValue: event.target.value, songs: [] }, this.searchSpotify);
+	// }
+
+	render() {
+		return <div>bar</div>;
+	}
+}
