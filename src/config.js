f2aeb7c0c32a612350cc77acef792476ce6ab425
Kyle Bremont <kylebremont@gmail.com>
2020-06-19 22:23:27 -0600

    changing api url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const apiUrl = 'https://35.232.73.166:3500/';
+export const apiUrl = 'http://35.232.73.166:3501/';
 export const clientId = '5c24a8e608774631812a8325b2b4057a';
 export const secret = '4837b819f7cd4cae8f4685c3b7906e5e';
