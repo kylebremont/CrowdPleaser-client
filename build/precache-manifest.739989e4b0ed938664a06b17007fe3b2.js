30fc7dd86837d3afea6989d1c00ae235e1e27449
Kyle Bremont <kylebremont@gmail.com>
2020-06-19 20:24:38 -0600

    adding build folder for deployment

---
+++ build/precache-manifest.739989e4b0ed938664a06b17007fe3b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5637cfe76c1a31a1a448",
+    "url": "/static/css/main.dbf0469a.chunk.css"
+  },
+  {
+    "revision": "5637cfe76c1a31a1a448",
+    "url": "/static/js/main.432552e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fd60a0ab72e26f483563",
+    "url": "/static/css/2.05d6c419.chunk.css"
+  },
+  {
+    "revision": "fd60a0ab72e26f483563",
+    "url": "/static/js/2.ab6492a4.chunk.js"
+  },
+  {
+    "revision": "210f0c0c8a8f27f103c2ed63309f4029",
+    "url": "/index.html"
+  }
+];
