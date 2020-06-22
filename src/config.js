2661aeeb10b738de914ec26ed48ecfcb465c0d91
Kyle Bremont <kylebremont@gmail.com>
2020-06-21 19:18:19 -0600

    changing api url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const apiUrl = 'http://35.224.46.104:3500/';
+export const apiUrl = 'https://crowdpleaser-280702.uc.r.appspot.com/';
 export const clientId = 'f0531284787841d89487e68e97f3c547';
 export const secret = 'c7bb46c0147c447ca857e1eb11460332';
